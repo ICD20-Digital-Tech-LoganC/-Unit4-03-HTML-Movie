4285869430bd13439d5a2900fe46c69ef07c7734
Logan-T-Connors <151658201+Logan-T-Connors@users.noreply.github.com>
2024-04-29 00:23:06 +0000

    final

--- js/script.js
+++ js/script.js
@@ -1,5 +1,29 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
-// This file contains the JS functions for index.html
+function calculateAge() {
+  let age = document.getElementById("userGuess").value;
+
+  if (age >= 18) {
+      document.getElementById("result").textContent = "You can watch all movies.";
+  } 
+  
+  else if (age >= 15) {
+      document.getElementById("result").textContent = "You can watch all movies except R rated movies.";
+  }
+
+  else if (age >= 13) {
+      document.getElementById("result").textContent = "You don't need parental guidance for PG-13 movies.";
+  }
+
+  else if (age >= 5) {
+      document.getElementById("result").textContent = "You can't watch PG movies alone.";
+  }
+  
+  else if (age >= 0) {
+      document.getElementById("result").textContent = "You can't watch movies alone.";
+  } 
+  
+  else {
+      document.getElementById("result").textContent = "Invalid age. Please enter a valid age.";
+  }
+
+  
+}
